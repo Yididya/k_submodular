b84869df3ab3df8a9c6802d3b3183809f9b0bade
Yididiya Nadew <yididiya@iastate.edu>
2023-03-23 21:11:21 -0500

    update diggs experiments

--- k_submodular/influence_maximization/experiment.py
+++ k_submodular/influence_maximization/experiment.py
@@ -6,39 +6,39 @@ import argparse
 
 from multiprocessing import Pool
 import numpy as np
-import networkx
+import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import ohsaka
 import threshold_algorithm
 
-import weighted_network
 import independent_cascade
 
 
 plt.rcParams['figure.figsize'] = [10,8]
 plt.rc('font', size = 30)
-plt.rc('legend', fontsize = 20)
+plt.rc('legend', fontsize = 22)
+
+def prepare_network():
+    with open('../../k_submodular/influence_maximization/diggs/diggs.pkl', 'rb') as f:
+        G = pickle.load(f)
+
+    users = list(G.nodes)
 
 
-
-class FriendDataset:
-
-    def __init__(self, file):
-        self.data = pd.read_csv(file, sep='\t')
-
-    @property
-    def users(self):
-        return list(self.data['user_id'].unique())
-    @property
-    def all_users(self):
-        return list(set(list(self.data['user_id'].unique()) + list(self.data['friend_id'].unique())))
-
-    def user_friends(self, user_id):
-        return self.data[self.data['user_id'] == user_id ]['friend_id'].unique()
+    return G, users
 
 
+def create_K_networks(network, K):
+
+    K_networks = [network.copy() for _ in range(K)]
+
+    for u, v in network.edges:
+        for i in range(K):
+            K_networks[i][u][v]['act_prob'] = K_networks[i][u][v][f'k_{i}']
+
+    return K_networks
 
 
 class Experiment:
@@ -47,10 +47,7 @@ class Experiment:
                  B_i,
                  topics,  # topic ids to spread,
                  tolerance=None,
-                 action_log_file='../../notebooks/output/digg_filtered_friends.txt',
-                 friend_list_file='../../notebooks/output/digg_filtered_friends.txt',
-                 weights_file='../../notebooks/output/digg_with_weights.txt_InfProbs(10)',
-                 n_mc=30,
+                 n_mc=50,
                  n_mc_final=10_000,
                  algorithm=ohsaka.KGreedyTotalSizeConstrained,
                  n_jobs=5
@@ -60,14 +57,12 @@ class Experiment:
         assert len(topics) == len(B_i), "#topics should be equal to the items to be selected"
 
         self.topics = topics # item id
-        self.action_log_file = action_log_file
-        self.friends_dataset = FriendDataset(friend_list_file)
+        self.network, self.active_nodes = prepare_network()
 
-        self.user_ids = self.friends_dataset.all_users
-        print(f'Using {len(self.user_ids)} users')
+        self._initialize_weighted_networks()
 
-
-        self.n = len(self.user_ids)
+        self.n = len(self.active_nodes)
+        print(f'Using {self.n} active users ')
         self.B_total = B_total # total budget
         self.B_i = B_i
 
@@ -78,13 +73,6 @@ class Experiment:
 
         print(f'Using {self.n_jobs} jobs, n_mc {self.n_mc}')
 
-
-        self.networks = {}
-        self.weights = {}
-
-        self._load_weights(weights_file)
-        self._initialize_weighted_networks()
-        print(f'Using K={len(self.networks.keys())} ')
 
         # initialize algorithm
         if self.tolerance is not None:
@@ -103,60 +91,62 @@ class Experiment:
 
 
     def _initialize_weighted_networks(self):
+        # load facebook network
+        self.K_networks = create_K_networks(self.network, len(self.topics))
 
-        G = networkx.DiGraph()
-        users = self.friends_dataset.users
 
-        for u in users:
-            friends = self.friends_dataset.user_friends(u)
-            G.add_edges_from([(u, f) for f in friends])
-
-        # Add weight to the network
-        for topic_id in self.topics:
-            G_topic = G.copy()
-            self.networks[topic_id] = weighted_network.weighted_network(G_topic, None, weights=self.weights[topic_id])
-            # self.networks[topic_id] = weighted_network.weighted_network(G_topic, 'rn')
-
-    def _load_weights(self, file):
-        data = pd.read_csv(file, sep='\t', names=['Influencer', 'Influenced'] + [f'Topic{t}' for t in self.topics], header=0, index_col=False)
-
-        for topic_id in self.topics:
-            topic_col = f'Topic{topic_id}'
-            sliced_data = data[['Influencer', 'Influenced', f'Topic{topic_id}']].to_dict('list')
-
-            influencer, influenced, weights_ = sliced_data['Influencer'], sliced_data['Influenced'], sliced_data[topic_col]
-
-            self.weights[topic_id]  = {(influencer[i], influenced[i]): weights_[i] for i, _ in enumerate(weights_)}
 
     def value_function(self, seed_set, n_mc=None):
         n_mc = n_mc or self.n_mc
-        infected_nodes = []
+        infected_nodes = {i:[] for i in range(n_mc)}
+        print(seed_set)
         for topic_idx, topic in enumerate(self.topics):
 
             # filter list of users by topic(item)
             # Translate the values
-            seed_t = [self.user_ids[location_idx] for item_idx, location_idx in seed_set if item_idx == topic_idx]
+            seed_t = [self.active_nodes[location_idx] for item_idx, location_idx in seed_set if item_idx == topic_idx]
 
             if seed_t:
-                # TODO: if this is the up
                 global ic_runner
                 def ic_runner(t):
-                    layers = independent_cascade.independent_cascade(self.networks[topic], list(set(seed_t)))
+                    layers = independent_cascade.independent_cascade(self.K_networks[topic_idx], list(set(seed_t)))
                     infected_nodes_ = [i for l in layers for i in l]
                     return infected_nodes_
 
                 with Pool(self.n_jobs) as p:
                     nodes = p.map(ic_runner, range(n_mc))
-                    nodes = [j for i in nodes for j in i]  # flatten
-                    infected_nodes.extend(nodes)
+                    for i, n in enumerate(nodes):
+                        infected_nodes[i].extend(n)
 
-        # Influences
-        return len(set(infected_nodes))
+        # Aggregate infected_nodes over MC runs
+        infected_nodes = np.mean([len(set(lst)) for lst in list(infected_nodes.values())])
+
+
+        return infected_nodes
 
 
     def run(self):
         for alg in self.algorithms:
             alg.run()
+
+
+    def final_run(self, S_list, n_mc=200):
+        """
+        Parameters
+        ----------
+        S_list list of selected values
+        Returns a dictionary with the evaluations of S on corresponding algorithms
+        ------
+        """
+        assert len(S_list) == len(self.algorithms), 'Number of algorithms and seed set do not match'
+        final_vals = []
+        for S in S_list:
+            final_vals.append(self.value_function(S, n_mc=n_mc))
+
+        return final_vals
+
+
+
     @property
     def results(self):
         return [{
@@ -175,10 +165,13 @@ class Experiment:
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Experiment runner')
-    parser.add_argument('--mode', action='store', type=str, default='plot', choices=['run', 'plot'])
-    parser.add_argument('--B', action='store', type=int, default=[ 5, 10, 15, 20, 25], nargs='+')
-    parser.add_argument('--n-jobs', action='store', type=int, default=20)
-    parser.add_argument('--tolerance', action='store', type=float, default=[0.1, 0.2, 0.3], nargs='+')
+    parser.add_argument('--mode', action='store', type=str, default='run', choices=['run', 'plot', 'final'])
+    # parser.add_argument('--B', action='store', type=int, default=[ 2, 4, 6, 8, 10, 12, 14, 16, 18, 20], nargs='+')
+    parser.add_argument('--B', action='store', type=int, default=[1, 2, 3, 4], nargs='+')
+
+    parser.add_argument('--n-jobs', action='store', type=int, default=10)
+    parser.add_argument('--n-mc', action='store', type=int, default=None)
+    parser.add_argument('--tolerance', action='store', type=float, default=[0.1, 0.2, 0.5], nargs='+') # TODO; update this
     parser.add_argument('--output', action='store', type=str, required=False)
     parser.add_argument('--alg', action='store', type=str, default=None,
                         choices=['KGreedyTotalSizeConstrained', 'KStochasticGreedyTotalSizeConstrained', 'ThresholdGreedyTotalSizeConstrained'])
@@ -189,6 +182,11 @@ if __name__ == '__main__':
     B_totals = args.B
     n_jobs = args.n_jobs
     tolerance_vals = args.tolerance
+    n_mc = args.n_mc or 1
+    n_mc_final = 500
+    # topics = range(1, 5)
+    topics = range(0, 10) #  ALL 10 TOPICS
+    print(f'Using Tolerance vals {tolerance_vals}, n_mc {n_mc}')
 
     # prepare directories
     output_dir = './output'
@@ -196,13 +194,13 @@ if __name__ == '__main__':
 
     alg_mappings = {
         'KGreedyTotalSizeConstrained': [ohsaka.KGreedyTotalSizeConstrained],
-        # 'KStochasticGreedyTotalSizeConstrained': [ohsaka.KStochasticGreedyTotalSizeConstrained],
+        'KStochasticGreedyTotalSizeConstrained': [ohsaka.KStochasticGreedyTotalSizeConstrained],
         'ThresholdGreedyTotalSizeConstrained': [threshold_algorithm.ThresholdGreedyTotalSizeConstrained]
     }
 
     algorithms = [
         ohsaka.KGreedyTotalSizeConstrained,
-        # ohsaka.KStochasticGreedyTotalSizeConstrained,
+        ohsaka.KStochasticGreedyTotalSizeConstrained,
         threshold_algorithm.ThresholdGreedyTotalSizeConstrained
     ]
 
@@ -213,23 +211,67 @@ if __name__ == '__main__':
         for alg in algorithms:
             for B_total in B_totals:
                 print(f'Running experiment for {alg} with budget {B_total}')
-                topics = list(range(1, 11))
-
+                print(f'Using topics {topics}')
                 exp = Experiment(
                     B_total=B_total,
                     B_i=[1] * len(topics),
                     topics=topics,
                     algorithm=alg,
-                    tolerance= tolerance_vals if 'Threshold' in alg.__name__ else None,
-                    n_jobs=n_jobs
+                    tolerance= tolerance_vals[:1] if 'Threshold' in alg.__name__ else None,
+                    n_jobs=n_jobs,
+                    n_mc=n_mc
                 )
-
 
                 exp.run()
 
                 # save file
-                with open(f'{output_dir}/{alg.__name__}__{B_total}_.pkl', 'wb') as f:
-                    pickle.dump(exp.results, f)
+                if 'Threshold' in alg.__name__:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_{tolerance_vals[0]}.pkl', 'wb') as f:
+                        pickle.dump(exp.results, f)
+                else:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_.pkl', 'wb') as f:
+                        pickle.dump(exp.results, f)
+    elif mode == 'final':
+        for alg in algorithms:
+            for B_total in B_totals:
+                # look at pickles
+                if 'Threshold' in alg.__name__:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_{tolerance_vals[0]}.pkl', 'rb') as f:
+                        results = pickle.load(f)
+                else:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_.pkl', 'rb') as f:
+                        results = pickle.load(f)
+
+                # if results[0].get('final_function_value', None):
+                #     print('Already calculated ')
+                #     continue
+
+                print(f'Running final run for {alg} with budget {B_total}')
+                print(f'Using topics {topics}')
+                exp = Experiment(
+                    B_total=B_total,
+                    B_i=[1] * len(topics),
+                    topics=topics,
+                    algorithm=alg,
+                    tolerance= tolerance_vals[:1] if 'Threshold' in alg.__name__ else None,
+                    n_jobs=n_jobs,
+                    n_mc=n_mc
+                )
+
+                final_vals = exp.final_run([r['S'] for r in results], n_mc=n_mc_final)
+                for k, r in enumerate(results):
+                    r['final_function_value'] = final_vals[k]
+
+
+                # update pickles
+                if 'Threshold' in alg.__name__:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_{tolerance_vals[0]}.pkl', 'wb') as f:
+                        pickle.dump(results, f)
+                else:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_.pkl', 'wb') as f:
+                        pickle.dump(results, f)
+
+
 
     elif mode == 'plot':
         # load the files
@@ -251,42 +293,51 @@ if __name__ == '__main__':
                 algs.append(alg)
 
             for B_total in B_totals:
-                with open(f'{output_dir}/{alg.__name__}__{B_total}_.pkl', 'rb') as f:
-                    results = pickle.load(f)
+                if 'Threshold' in alg.__name__:
+                    results = []
+                    for t_val in tolerance_vals:
+                        with open(f'{output_dir}/{alg.__name__}__{B_total}_{t_val}.pkl', 'rb') as f:
+                            results.extend(pickle.load(f))
+                else:
+                    with open(f'{output_dir}/{alg.__name__}__{B_total}_.pkl', 'rb') as f:
+                        results = pickle.load(f)
 
-                if type(results) == dict: results = [results]
+                # if type(results) == dict: results = [results]
 
                 for i, r in enumerate(results):
                     if 'Threshold' in alg.__name__:
                         name = alg.name + f'($\epsilon$={tolerance_vals[i]})'
-                        function_values[name].append(r['function_value'])
+                        function_values[name].append(r.get('final_function_value', r['function_value']))
                         n_evaluations[name].append(r['n_evals'])
                     else:
 
-                        function_values[alg.name].append(r['function_value'])
+                        function_values[alg.name].append(r.get('final_function_value', r['function_value']))
                         n_evaluations[alg.name].append(r['n_evals'])
 
 
         marker_types = ['o', 'v', '*', 'D', 's']
         for i, key in enumerate(function_values.keys()):
-            plt.plot(range(len(B_totals)), function_values[key], label=key, marker=marker_types[i])
-            plt.ylabel('Influence spread')
-            plt.xlabel('Budget (B)')
-            plt.xticks(range(len(B_totals)), B_totals)
+            plt.plot(range(len(B_totals)), function_values[key], label=key, marker=marker_types[i], markersize=12)
 
+            plt.xticks(range(len(B_totals)), B_totals)
+        plt.ylabel('Influence Spread')
+        plt.xlabel('Total Budget')
+        plt.grid(axis='both')
         plt.legend()
 
-        plt.savefig(f'{output_dir}/figure-infected-nodes.png', dpi=300, bbox_inches='tight')
+        plt.savefig(f'{output_dir}/TS-influence-n51-k4.png', dpi=300, bbox_inches='tight')
         plt.show()
         plt.figure()
 
         for i, key in enumerate(function_values.keys()):
-            plt.plot(range(len(B_totals)), n_evaluations[key], label=key, marker=marker_types[i])
-            plt.ylabel('function evaluations')
+            plt.plot(range(len(B_totals)), n_evaluations[key], label=key, marker=marker_types[i], markersize=12)
+
             plt.xticks(range(len(B_totals)), B_totals)
-            plt.xlabel('Total Size (b)')
+            plt.ylabel('Function Evaluations')
+            plt.xlabel('Total Budget')
+        plt.grid(axis='both')
         plt.legend()
 
-        plt.savefig(f'{output_dir}/figure-function-evaluations.png', dpi=300, bbox_inches='tight')
+        plt.savefig(f'{output_dir}/TS-eval-n51-k4.png', dpi=300, bbox_inches='tight')
         plt.show()
         plt.figure()
